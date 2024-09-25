6191ff5f516fa32a4d9ac9fb43cd611744c81838
tanapp <tann2682004@gmail.com>
2024-09-25 17:54:47 +0700

    noi dung

--- App.js
+++ App.js
@@ -1,20 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+      backgroundColor: '#fff',
+    };
+  }
+
+  handleButtonClick = (newMessage, newColor) => {
+    this.setState({ message: newMessage, backgroundColor: newColor });
+    Alert.alert(newMessage);
+  };
+
+  render() {
+    return (
+      <View style={[styles.container, { backgroundColor: this.state.backgroundColor }]}>
+        
+        <ButtonComponent
+          backgroundColor="green"
+          message="Green button clicked!"
+          onClick={this.handleButtonClick}
+          colorChange="#32CD32" // limegreen
+          label="Green"
+        />
+        <ButtonComponent
+          backgroundColor="red"
+          message="Red button clicked!"
+          onClick={this.handleButtonClick}
+          colorChange="#FF6347" // tomato
+          label="Red"
+        />
+        <ButtonComponent
+          backgroundColor="black"
+          message="Black button clicked!"
+          onClick={this.handleButtonClick}
+          colorChange="#000000" // black
+          label="Black"
+        />
+        <ButtonComponent
+          backgroundColor="yellow"
+          message="Yellow button clicked!"
+          onClick={this.handleButtonClick}
+          colorChange="#FFFF00" // yellow
+          label="Yellow"
+        />
+        <ButtonComponent
+          backgroundColor="brown"
+          message="Brown button clicked!"
+          onClick={this.handleButtonClick}
+          colorChange="#A52A2A" // brown
+          label="Brown"
+        />
+        <ButtonComponent
+          backgroundColor="blue"
+          message="Blue button clicked!"
+          onClick={this.handleButtonClick}
+          colorChange="#0000FF" // blue
+          label="Blue"
+        />
+
+        {this.state.message && <Text style={styles.message}>{this.state.message}</Text>}
+      </View>
+    );
+  }
+}
+
+class ButtonComponent extends Component {
+  render() {
+    const { backgroundColor, message, onClick, colorChange, label } = this.props;
+    return (
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: backgroundColor }]}
+        onPress={() => onClick(message, colorChange)}
+      >
+        <Text style={styles.buttonText}>{label}</Text>
+      </TouchableOpacity>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    width:'90%',
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+   textAlign:'center',
+  },
+  message: {
+    marginTop: 20,
+    fontSize: 18,
+    color: 'gray',
   },
 });
+
+export default App;
